9d8bc78f2d46d32dec0b18bf009289cc60c47146
Luis Decena <ovando.decena.luis@gmail.com>
2023-09-27 22:43:28 +0200

    sa

--- src/main/java/common/Constants.java
+++ src/main/java/common/Constants.java
@@ -10,29 +10,5 @@ public class Constants {
     public static final String THE_CUSTOMER_LIST_IS_EMPTY_S = "The customer list is empty";
     public static final String THE_ORDER_LIST_IS_EMPTY = "The order list is empty";
     public static final String THERE_IS_AN_EMPTY_FIELD = "There is an empty field.";
-    public static final String CUSTOMER_ADDED = "Customer added";
-    public static final String CUSTOMER_ADDED_SUCCESSFULLY = "Customer added successfully.";
-    public static final String ERROR = "Error";
-    public static final String ERROR_ADDING_CUSTOMER = "Error adding customer.";
-    public static final String FAILED_TO_UPDATE_THE_CUSTOMER = "Failed to update the customer.";
-    public static final String INCORRECT_USER_OR_PASSWORD = "Incorrect user or password";
-    public static final String ERROR_ADDING_ORDER = "Error adding order.";
-    public static final String ITEM_ADDED = "Item added";
-    public static final String ITEM_ADDED_SUCCESSFULLY = "Item added successfully.";
-    public static final String ITEM_REMOVED = "Item removed";
-    public static final String ITEM_REMOVED_SUCCESSFULLY = "Item removed successfully.";
-    public static final String ORDER_ADDED = "Order added";
-    public static final String ORDER_ADDED_SUCCESSFULLY = "Order added successfully.";
-    public static final String ORDER_DELETED = "Order Deleted";
-    public static final String THE_ORDER_HAS_BEEN_DELETED_SUCCESSFULLY = "The order has been deleted successfully.";
-    public static final String FAILED_TO_DELETE_THE_ORDER = "Failed to delete the order.";
-    public static final String TO_DELETE_AN_ORDER_SELECT_IT_FIRST = "To delete an order select it first.";
-    public static final String SELECT_AN_ORDER = "Select an order";
-    public static final String ORDER_UPDATED_SUCCESSFULLY = "Order updated successfully.";
-    public static final String ERROR_UPDATING_CLIENT = "Error updating client.";
-    public static final String CUSTOMER_DELETED = "Customer Deleted";
-    public static final String THE_CUSTOMER_HAS_BEEN_DELETED_SUCCESSFULLY = "The customer has been deleted successfully.";
-    public static final String FAILED_TO_DELETE_THE_CUSTOMER = "Failed to delete the customer.";
-    public static final String QUIT_APPLICATION = "Quit application";
-    public static final String CLOSE_WITHOUT_SAVING = "Close without saving?";
+
 }
